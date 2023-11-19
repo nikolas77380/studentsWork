3a524f6b475217a24c0024eb59cc14c4669d2688
Sergiy199102 <142042867+Sergiy199102@users.noreply.github.com>
2023-11-19 13:39:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuComponent from './MenuComponent';
+
+const App = () => {
+    return (
+        <div>
+            {/* Другие компоненты вашего приложения */}
+            <MenuComponent />
+        </div>
+    );
+};
+
+export default App;
